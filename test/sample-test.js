c114b06503f9c83ebf42ae65d00cf9bb0814c3b7
Loïs <snowin71@outlook.fr>
2021-12-02 17:07:27 +0100

    first commit with renamed forked UniswapV2 contracts

---
+++ test/sample-test.js
@@ -0,0 +1,19 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Greeter", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const AggregatorV3Interface = await ethers.getContractFactory("AggregatorV3Interface");
+    const aggregator = await AggregatorV3Interface.deploy("Deployement of aggregator...");
+    await aggregator.deployed();
+
+    expect(await greeter.greet()).to.equal("Hello, world!");
+
+    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+    // wait until the transaction is mined
+    await setGreetingTx.wait();
+
+    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+});
