7a64b0f293801ad55d219086be911496c31b9dc0
Loïs <snowin71@outlook.fr>
2021-12-04 01:51:53 +0100

    extras changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,7 +22,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.10",
+  solidity: {
+    version: "0.8.10",
+    /** we should enable optimizer to prevent router contract being too big to deploy
+     *  we expect contracts to be executed more than millions times.
+     */
+    settings: {
+      optimizer: {enabled: true, runs: 1000000},
+    }
+  },
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
