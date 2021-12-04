284a7d2afc368c1f6a96e3a2cd9f3782f6c586a5
Loïs <snowin71@outlook.fr>
2021-12-05 00:50:55 +0100

    Added comments and events

--- contracts/3_StakingContract/LLStaking.sol
+++ contracts/3_StakingContract/LLStaking.sol
@@ -6,10 +6,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LLStaking is ERC20 {
 
+    /** @notice the staking token */
     IERC20 public lls;
 
     /** @dev timestamp of last mint */
     uint private lastMint;
+
+    event onDeposit(uint _amountLLS, uint _amountShares, address _from);
+    event onWithdraw(uint _amountShares, uint _amountLLS, address _to);
 
     constructor(address _lls) ERC20("staked LLS", "stkLLS") {
         lls = IERC20(_lls);
@@ -19,6 +23,10 @@ contract LLStaking is ERC20 {
         stake(lls.balanceOf(msg.sender));
     }
 
+    /**
+        @notice transfer LLS on this contract and give freshly created stkLLS in exchange 
+        @dev stkLLS represents shares 
+    */
     function stake(uint _amount) public {
         require(_amount > 0, "LLStaking: null amount");
         lls.transferFrom(msg.sender, address(this), _amount);
@@ -30,20 +38,26 @@ contract LLStaking is ERC20 {
             shares = _amount * (totalSupply() / lls.balanceOf(address(this)));
         }
         _mint(msg.sender, shares);
+        emit onDeposit(_amount, shares, msg.sender);
     }
 
     function unstakeAll() external {
         unstake(balanceOf(msg.sender));
     }
 
+    /**
+        @notice burn stkLLS and give back LLS depending on burned shares
+    */
     function unstake(uint _shares) public {
         require(_shares > 0, "LLStaking: null shares");
         uint amount = _shares / (totalSupply() * lls.balanceOf(address(this)));
         _burn(msg.sender, _shares); 
         lls.transfer(msg.sender, amount);
+        emit onWithdraw(_shares, amount, msg.sender);
     }
 
-    function mintLLS() external {
+    /** @notice mint some LLS rewards */
+    function mintLLS() public {
         uint timeCheck = block.timestamp - lastMint;
         require(timeCheck >= 3600, "LLStaking: should wait for new mint");
         (bool success, bytes memory data) = address(lls).call(abi.encodeWithSignature("mint(address,uint)", address(this), 10*(10**18)));
